74b91ce0b216814d6f225aaca226fb84db3b9083
xiaoDao776 <3884075394@qq.com>
2026-08-19 08:23:23 +0800

    Update ModuleManager.kt

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
@@ -215,6 +215,7 @@ import net.ccbluex.liquidbounce.features.module.modules.render.ModuleMobOwners
 import net.ccbluex.liquidbounce.features.module.modules.render.ModuleNewChunks
 import net.ccbluex.liquidbounce.features.module.modules.render.ModuleNoBob
 import net.ccbluex.liquidbounce.features.module.modules.render.ModuleNoFov
+import net.ccbluex.liquidbounce.features.module.modules.render.ModuleRiseClickgui
 import net.ccbluex.liquidbounce.features.module.modules.render.ModuleNoHurtCam
 import net.ccbluex.liquidbounce.features.module.modules.render.ModuleNoSwing
 import net.ccbluex.liquidbounce.features.module.modules.render.ModuleParticles
@@ -648,6 +649,7 @@ object ModuleManager : EventListener, Collection<ClientModule> by modules {
             ModuleFreeCam,
             ModuleSmoothCamera,
             ModuleFreeLook,
+            ModuleRiseClickgui,
             ModuleFullBright,
             ModuleHoleESP,
             ModuleHud,
